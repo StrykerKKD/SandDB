190a84cda2336ad34fb9b15c7a04d757762ed08a
Tóth Róbert <kkdstryker@gmail.com>
2018-04-18 22:55:29 +0200

    Added serializer.

--- lib/sanddb.ml
+++ lib/sanddb.ml
@@ -34,6 +34,20 @@ let database_read_records (type a) file_path serializer =
   let records = List.map Serializer.t_of_string raw_records in
   Lwt.return records
 
+let serialize_record_data (type a) record_data record_data_serializer =
+  let open Serializer_converter in
+  let module Record_Data_Serializer = (val record_data_serializer : Serializer with type t = a) in
+  Record_Data_Serializer.string_of_t record_data
+
+let serialize_record record_data record_serializer record_data_serializer =
+  let open Serializer_converter in
+  let open Record_t in
+  let module Record_Serializer = (val record_serializer : Serializer with type t = Record_t.t) in
+  let serialized_record_data = serialize_record_data record_data record_data_serializer in
+  let id = Uuidm.v `V4 |> Uuidm.to_string in
+  let record = {id = id; data = serialized_record_data} in
+  Record_Serializer.string_of_t record
+
 let database_insert_record (type a) file_path serializer record =
   let open Serializer_converter in
   let module Serializer = (val serializer : Serializer with type t = a) in
