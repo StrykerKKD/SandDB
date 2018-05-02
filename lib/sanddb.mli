a21372433fb30f7ad07a477a26526dc83d201cbe
Tóth Róbert <kkdstryker@gmail.com>
2018-05-02 21:03:20 +0200

    Fixing unbound lwt_io nonsense. Fixing up interface.

--- lib/sanddb.mli
+++ lib/sanddb.mli
@@ -39,25 +39,23 @@ end
  - [json_serializer] which will be responsible with the serialization between the database and the client
  This function will return a first class database module, which can be used to communicate with the database.
 *)
-val create_json_database : string -> (module Serializers.Json_Serializer with type t = 'a) -> (module Database with type t = 'a)
+val create_json_database : Lwt_io.file_name -> (module Serializers.Json_Serializer with type t = 'a) -> (module Database with type t = 'a)
 
 (** [Sanddb.create_biniou_database file_name biniou_serializer] will create a biniou database based on the provided:
  - [file_name] which will be tha base of the database
  - [biniou_serializer] which will be responsible with the serialization between the database and the client
  This function will return a first class database module, which can be used to communicate with the database.
 *)
-val create_biniou_database : string -> (module Serializers.Biniou_Serializer with type t = 'a) -> (module Database with type t = 'a)
+val create_biniou_database : Lwt_io.file_name -> (module Serializers.Biniou_Serializer with type t = 'a) -> (module Database with type t = 'a)
 
 (** [Sanddb.read_all_records database unit] gives back every database record, both visible and shadowed ones. The result can contain duplicate record ids.
 The first record in the list is the oldest record and the last one is the newest record.
-Creates the database file if it doesn't exists.
-Throws exception if the file is empty.*)
+Creates the database file if it doesn't exists.*)
 val read_all_records : (module Database with type t = 'a) -> unit -> (Record_Id.t * 'a, exn) result list Lwt.t
 
 (** [Sanddb.read_visible_records database unit] gives back every visible(not shadowed) database record, which also means there is no duplicate record id in the result.
 The first record in the list is the newest record and the last one is the oldest record.
-Creates the database file if it doesn't exists.
-Throws exception if the file is empty.*)
+Creates the database file if it doesn't exists.*)
 val read_visible_records : (module Database with type t = 'a) -> unit -> (Record_Id.t * 'a, exn) result list Lwt.t
 
 (** [Sanddb.insert_record database data] inserts record into the database. The record id is generated automatically and given back as a result.
