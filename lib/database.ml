2d5755e617eb926e146daf1817dd9d8ffa3ea088
Tóth Róbert <kkdstryker@gmail.com>
2018-09-19 22:24:35 +0200

    Refactor out record_serializer and file_io from database module.

--- lib/database.ml
+++ lib/database.ml
@@ -7,38 +7,10 @@ module type T = sig
   val insert_shadowing_record : Record_id.t -> t -> Record_id.t Lwt.t
 end
 
-let deserialize_record_data (type a) record_data_serializer record_data =
-  let module Record_Data_Serializer = (val record_data_serializer : Serializer.Generic_serializer with type t = a) in
-  Record_Data_Serializer.t_of_string record_data
-
-let deserialize_record_content record_data_serializer deserialized_record = 
-  let open Record_t in
-  let record_id = Base.Result.of_option ~error: (Failure "Record id parsing failed") (Record_id.of_string deserialized_record.id) in
-  let record_data = Base.Result.try_with (fun () -> deserialize_record_data record_data_serializer deserialized_record.data) in
-  match record_id, record_data with
-  | Ok id, Ok data -> Ok (id, data)
-  | Error _ as error ,  _ -> error
-  | _ , (Error _ as error) -> error
-
-let deserialize_record record_serializer record_data_serializer record =
-  let module Record_Serializer = (val record_serializer : Serializer.Generic_serializer with type t = Record_t.t) in
-  let unescaped_record = Caml.Scanf.unescaped record in
-  let deserialized_record = Base.Result.try_with (fun () -> Record_Serializer.t_of_string unescaped_record)  in
-  Base.Result.bind deserialized_record ~f:(deserialize_record_content record_data_serializer)
-
-let deserialize_records record_serializer record_data_serializer raw_data =
-  let cleaned_raw_data = String.trim raw_data in
-  let raw_records = String.split_on_char '\n' cleaned_raw_data in
-  let serializer = deserialize_record record_serializer record_data_serializer in
-  Base.List.map ~f:serializer raw_records
-
-let read_from_file file_path = 
-  Lwt_io.with_file ~mode: Input file_path (fun channel -> Lwt_io.read channel)
-
 let database_read_all_records file_path record_serializer record_data_serializer =
   let open Lwt.Infix in
-  read_from_file file_path >>= fun raw_data ->
-  let records = deserialize_records record_serializer record_data_serializer raw_data in
+  File_io.read_from_file file_path >>= fun raw_data ->
+  let records = Record_serializer.deserialize_records record_serializer record_data_serializer raw_data in
   Lwt.return records
 
 let filter_duplicate_record record (unique_record_ids, accumulator) =
@@ -58,37 +30,17 @@ let database_read_visible_records file_path record_serializer record_data_serial
       ~init:(Base.Set.empty (module Record_id), []) in
   Lwt.return visible_records
 
-let serialize_record_data (type a) record_data_serializer record_data =
-  let module Record_Data_Serializer = (val record_data_serializer : Serializer.Generic_serializer with type t = a) in
-  Record_Data_Serializer.string_of_t record_data
-
-let serialize_record record_serializer record_data_serializer record_id record_data =
-  let open Record_t in
-  let module Record_Serializer = (val record_serializer : Serializer.Generic_serializer with type t = Record_t.t) in
-  let serialized_record_data = serialize_record_data record_data_serializer record_data in
-  let serialized_id = Record_id.to_string record_id in
-  let record = {id = serialized_id; data = serialized_record_data} in
-  let serialized_record = Record_Serializer.string_of_t record in 
-  Caml.String.escaped serialized_record
-
-let append_to_file file_path serialized_record =
-  Lwt_io.with_file 
-    ~flags:([Unix.O_WRONLY; Unix.O_NONBLOCK; Unix.O_APPEND; Unix.O_CREAT]) 
-    ~mode: Output 
-    file_path 
-    (fun channel -> Lwt_io.write_line channel serialized_record)
-
 let database_insert_record file_path record_serializer record_data_serializer record_data =
   let open Lwt.Infix in
   let record_id = Record_id.v `V4 in 
-  let serialized_record = serialize_record record_serializer record_data_serializer record_id record_data in
-  append_to_file file_path serialized_record >>= fun () ->
+  let serialized_record = Record_serializer.serialize_record record_serializer record_data_serializer record_id record_data in
+  File_io.append_to_file file_path serialized_record >>= fun () ->
   Lwt.return record_id
 
 let database_insert_shadowing_record file_path record_serializer record_data_serializer record_id record_data =
   let open Lwt.Infix in
-  let serialized_record = serialize_record record_serializer record_data_serializer record_id record_data in
-  append_to_file file_path serialized_record >>= fun () ->
+  let serialized_record = Record_serializer.serialize_record record_serializer record_data_serializer record_id record_data in
+  File_io.append_to_file file_path serialized_record >>= fun () ->
   Lwt.return record_id
 
 let create_database_module (type a) file_path record_serializer record_data_serializer = 
